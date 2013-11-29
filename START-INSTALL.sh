53d0010233d4012907b580bcf808b7b452b2ed4c
ghoumard <ghoumard@gmail.com>
2013-11-29 12:57:26 -0500

    Use .tar.gz instead of zip from github
    Use .tar.gz instead of zip from github

--- START-INSTALL.sh
+++ START-INSTALL.sh
@@ -5,8 +5,8 @@ echo "DEBUT DE L'INSTALLATION"
 mkdir /cedromsni
 cd /cedromsni
 
-wget https://github.com/querbes/querbes/archive/master.zip
-unzip master.zip
+wget https://github.com/querbes/querbes/archive/master.tar.gz
+tar -zxvf master.tar.gz
 mv querbes-master/ install/
 cd install
 
