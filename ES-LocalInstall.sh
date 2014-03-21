7b75142f95b5f0515cfb2e6d8ae9a36ed0d88740
ghoumard <ghoumard@gmail.com>
2014-03-21 10:40:28 -0400

    fix LocalInstall script
    fix LocalInstall script

--- ES-LocalInstall.sh
+++ ES-LocalInstall.sh
@@ -1,12 +1,12 @@
  #!/bin/sh
-if ([ $# -ne 7 ] )
-then 
+if ([ $# -ne 5 ] )
+then
     if( [ $# -eq 1 ]  && [ $1 = '--help' ] )
     then
         echo ""
         echo "Usage: ES-LocalInstall.sh 'MyPa$sw0rd' 'Collogix' '30g' '30g' '512m'"
         echo "Usage: ES-LocalInstall.sh [user] [IP] [NginxPassword] [SiteName] [HeapSize_A] [HeapSize_B] [HeapSize_M]"
-        echo "       NGinx Password: Password for the nginx proxy (ex: 'MyPa$sw0rd')" 
+        echo "       NGinx Password: Password for the nginx proxy (ex: 'MyPa$sw0rd')"
         echo "       SiteName: DataCenter Name (ex: 'Collogix' or 'Querbes')"
         echo "       HeapSize_A: Node A HeapSize memory (ex: '30g')"
         echo "       HeapSize_B: Node B HeapSize memory (ex: '30g')"
